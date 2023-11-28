bd18cc8efdfb3aa6fc0e1944b062b1c4afbed598
bananaVG <78326537+bananaVG@users.noreply.github.com>
2023-11-27 17:26:15 -0800

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,94 @@
+import cv2
+
+cv2.imread("solar-system.jpg")
+cv2.imshow("output",img)
+
+cv2.putText(img,
+            "Sun",
+            (20,300)
+            cv2.FONT_HERSHER_COMPLEX,
+            o.5
+            (255,255,255)
+        
+
+)
+
+cv2.putText(img,
+            "Mercury",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (255,0,0)
+        
+
+)
+
+cv2.putText(img,
+            "Venus",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (0,255,0)
+        
+
+)
+
+cv2.putText(img,
+            "Earth",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (0,0,255)
+        
+
+)
+
+cv2.putText(img,
+            "Mars",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (254,255,255)
+        
+
+)
+
+cv2.putText(img,
+            "Jupiter",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (255,254,255)
+        
+
+)
+
+cv2.putText(img,
+            "Saturn",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (255,255,254)
+        
+
+)
+
+cv2.putText(img,
+            "Uranus",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (253,255,255)
+        
+
+)
+
+cv2.putText(img,
+            "Neptune",
+            (20,300)
+            cv2.FONT_HERSHEY_COMPLEX,
+            o.5
+            (255,253,255)
+        
+
+)
